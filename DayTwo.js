927a1862d7191f52d05123d168f9373260fd88b0
Jan Krumrein <hakisuj@gmail.com>
2021-06-28 12:40:32 +0200

    :art: use function to dry

--- DayTwo.js
+++ DayTwo.js
@@ -5,26 +5,58 @@ const fs = require('fs')
 const encoded = fs.readFileSync("/Users/hakizu/Downloads/message.txt")
 const input = encoded.toString('utf-8').split('\n')
 
+//Part1
 const validPasswords = input.filter(it => {
-    const splitSegements = it.split(':')
-    const policy = splitSegements[0]
-    const password = splitSegements[1].slice(1) //remove whitespace
 
-    const policySegments = policy.split(' ')
-    const policyNumber = policySegments[0]
-    const policyChar = policySegments[1]
+    const {first: policy, second: password} = splitSegments(it, ':', true)
+    const {first: policyNumber, second: policyChar} = splitSegments(policy, ' ')
+    const {first: policyNumberMin, second: policyNumberMax} = 
+        splitSegments(policyNumber, '-', false, true)
 
     const regex = new RegExp(policyChar, "g")
     const matchedChars = password.match(regex)
     const numberOfMatches = matchedChars?.length
 
-    const policyNumberSegments = policyNumber.split('-')
-    const policyNumberMax = parseInt(policyNumberSegments[1])
-    const policyNumberMin = parseInt(policyNumberSegments[0])
-
-    const passwordIsValid = policyNumberMin <= numberOfMatches &&
+    const passwordIsValid = 
+        policyNumberMin <= numberOfMatches &&
         numberOfMatches <= policyNumberMax
     return passwordIsValid
 })
-
 console.log(validPasswords.length)
+
+
+function splitSegments(arg, split, slice, parse) {
+    const segments = arg.split(split)
+    if (slice) {
+        const slicedSegments = [segments[0], segments[1].slice(1)]
+        return {first: slicedSegments[0], second: slicedSegments[1]}
+    }
+    if (parse) {
+        const numberSegments = segments.map(it => parseInt(it))
+        return {first : numberSegments[0], second: numberSegments[1]}
+    }
+    return {first: segments[0], second: segments[1]}
+}
+
+
+//Part2
+const partTwo = input.filter(it => {
+    const {first: policy, second: password} = splitSegments(it, ':', true)
+    const {first: policyNumber, second: policyChar} = splitSegments(policy, ' ')
+    const {first: policyNumberMin, second: policyNumberMax} = 
+        splitSegments(policyNumber, '-', false, true)
+
+    const firstIndex = policyNumberMin - 1
+    const secondIndex = policyNumberMax - 1
+
+    if (password[firstIndex] === policyChar ||
+        password[secondIndex] === policyChar ||
+        password[secondIndex] === undefined) {
+
+            const isInvalid = password[firstIndex] === policyChar &&
+                password[secondIndex] === policyChar
+            return !isInvalid
+        }
+})
+
+console.log(partTwo.length)
